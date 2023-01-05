53b7b3e3cd2acd290bde8d7172293c2f2ebbeb9c
Maxime Koyamba <mkoyamba@Maximes-iMac.local>
2023-01-05 13:15:41 +0100

    done

--- src/exec/raycasting_utils.c
+++ src/exec/raycasting_utils.c
@@ -6,7 +6,7 @@
 /*   By: mkoyamba <mkoyamba@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 15:02:41 by mkoyamba          #+#    #+#             */
-/*   Updated: 2022/12/19 15:30:05 by mkoyamba         ###   ########.fr       */
+/*   Updated: 2023/01/05 13:08:57 by mkoyamba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,73 +14,73 @@
 
 float	return_check_se(t_map *map, float raydir, float *x, float *y)
 {
-	if (((map->ray_len_V < map->ray_len_H)
-			|| map->ray_len_H == -1) && map->ray_len_V != -1)
+	if (((map->ray_len_v < map->ray_len_h)
+			|| map->ray_len_h == -1) && map->ray_len_v != -1)
 	{
-		*y = map->player.y + map->ray_len_V * sin((raydir - 1) * M_PI_2);
+		*y = map->player.y + map->ray_len_v * sin((raydir - 1) * M_PI_2);
 		map->wall_pos = *y - (int)*y;
 		map->wall_color = WEST;
-		return (map->ray_len_V);
+		return (map->ray_len_v);
 	}
 	else
 	{
 		map->wall_pos = *x - (int)*x;
 		map->wall_color = NORTH;
-		return (map->ray_len_H);
+		return (map->ray_len_h);
 	}
 }
 
 float	return_check_sw(t_map *map, float raydir, float *x, float *y)
 {
-	if (((map->ray_len_V < map->ray_len_H)
-			|| map->ray_len_H == -1) && map->ray_len_V != -1)
+	if (((map->ray_len_v < map->ray_len_h)
+			|| map->ray_len_h == -1) && map->ray_len_v != -1)
 	{
-		*y = map->player.y + map->ray_len_V * cos((raydir - 2) * M_PI_2);
+		*y = map->player.y + map->ray_len_v * cos((raydir - 2) * M_PI_2);
 		map->wall_pos = *y - (int)*y;
 		map->wall_color = EAST;
-		return (map->ray_len_V);
+		return (map->ray_len_v);
 	}
 	else
 	{
 		map->wall_pos = *x - (int)*x;
 		map->wall_color = NORTH;
-		return (map->ray_len_H);
+		return (map->ray_len_h);
 	}
 }
 
 float	return_check_ne(t_map *map, float raydir, float *x, float *y)
 {
-	if (((map->ray_len_V < map->ray_len_H)
-			|| map->ray_len_H == -1) && map->ray_len_V != -1)
+	if (((map->ray_len_v < map->ray_len_h)
+			|| map->ray_len_h == -1) && map->ray_len_v != -1)
 	{
-		*y = map->player.y - map->ray_len_V * cos((raydir) * M_PI_2);
+		*y = map->player.y - map->ray_len_v * cos((raydir) * M_PI_2);
 		map->wall_pos = *y - (int)*y;
 		map->wall_color = WEST;
-		return (map->ray_len_V);
+		return (map->ray_len_v);
 	}
 	else
 	{
 		map->wall_pos = *x - (int)*x;
 		map->wall_color = SOUTH;
-		return (map->ray_len_H);
+		return (map->ray_len_h);
 	}
 }
 
 float	return_check_nw(t_map *map, float raydir, float *x, float *y)
 {
-	if (((map->ray_len_V < map->ray_len_H)
-			|| map->ray_len_H == -1) && map->ray_len_V != -1)
+	if (((map->ray_len_v < map->ray_len_h)
+			|| map->ray_len_h == -1) && map->ray_len_v != -1)
 	{
-		*y = map->player.y - map->ray_len_V * sin((raydir - 3) * M_PI_2);
+		*y = map->player.y - map->ray_len_v * sin((raydir - 3) * M_PI_2);
 		map->wall_pos = *y - (int)*y;
 		map->wall_color = EAST;
-		return (map->ray_len_V);
+		return (map->ray_len_v);
 	}
 	else
 	{
 		map->wall_pos = *x - (int)*x;
 		map->wall_color = SOUTH;
-		return (map->ray_len_H);
+		return (map->ray_len_h);
 	}
 }
 
